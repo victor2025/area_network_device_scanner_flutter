cc4ab80609f992ca137eb0fae928d9f3480602cc
victor2022_Ubuntu_LAPTOP <lihengwei111@outlook.com>
2023-04-17 14:49:02 +0800

    完善了打包脚本

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -9,8 +9,8 @@ aim_path=$3/android/$version
 res_path="$(pwd)"/build/app/outputs/apk/release
 v7a=armeabi-v7a
 v8a=arm64-v8a
-v7a_name="$app_name"-v7a-"$version".apk
-v8a_name="$app_name"-v8a-"$version".apk
+v7a_name="$app_name"-armv7-"$version".apk
+v8a_name="$app_name"-armv8-"$version".apk
 # build
 flutter build apk --target-platform android-arm,android-arm64 --split-per-abi --release --obfuscate --split-debug-info=obfuscate/debug-info --tree-shake-icons
 # move
